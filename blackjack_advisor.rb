5e846c08bd794b65da8bc34af71298488558d44e
amax3002 <alexandremguy@gmil.com>
2016-10-17 15:51:05 -0400

    updated version

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -12,22 +12,19 @@ type_of_user_hand = ""
 
 ############################ defining methods
 
-def user_hand_first(first)
-  first = gets.chomp.to_i
-  return first
+def get_card(choice)
+  puts "Please enter your first card: "
+  choice = gets.chomp
+  case choice
+  when "J","Q","K","j","q","k"
+    return 10
+  when "A","a"
+    return 11
+  else
+    return choice.to_i
+  end
 end
 
-
-def user_hand_second(second)
-  second = gets.chomp.to_i
-  return second
-end
-
-
-def user_hand_dealer(dealer_card)
-  dealer_card = gets.chomp.to_i
-  return dealer_card
-end
 
 
 def total_user_value(first_value, second_value)
@@ -39,7 +36,7 @@ def hand_type(check_first_card, check_second_card)
   hand_type = ""
   if check_first_card == check_second_card
     hand_type = "Pair"
-  elsif check_first_card == 1 && check_second_card != 1 || check_first_card != 1 && check_second_card == 1
+  elsif check_first_card == 11 && check_second_card != 11 || check_first_card != 11 && check_second_card == 11
     hand_type = "Soft"
   else
     hand_type = "Hard"
@@ -48,7 +45,7 @@ def hand_type(check_first_card, check_second_card)
 end
 
 
-def choose_table(type_chose, hard_pair, soft_pair, pair_pair)
+def choose_tables(type_chose, hard_pair, soft_pair, pair_pair)
   case type_chose
   when  "Hard"
     return hard_pair
@@ -61,38 +58,54 @@ def choose_table(type_chose, hard_pair, soft_pair, pair_pair)
   end
 end
 
+def subtract_amount (type)
+  if type == "Pair"
+    2
+  elsif type =="Hard"
+    5
+  else
+    13
+  end
+end
+
+def subtract_from_choice (type,one_card,two_card)
+  if type == "Pair"
+    one_card
+  else
+    two_card
+  end
+end
+
 # ___What I need to DO___
-# =>  1. Make Ace, King, Q, J numbers
-# =>  2. Fix Pair choices.
 
 
 ########################## code
 
 # sets up user cards
-first_card = user_hand_first(first_card)
-second_card = user_hand_second(second_card)
+first_card = get_card(first_card)
+second_card = get_card(second_card)
 total_card_value = total_user_value(first_card, second_card)
 
 # dealer first card
-dealer_first_card = user_hand_dealer(dealer_first_card)
+dealer_first_card = get_card(dealer_first_card)
 
 type_of_user_hand = hand_type(first_card, second_card)
 
 
-puts ''
+puts '_'*50
 puts "Total user card value: #{total_card_value}"
 
 puts "1st card: #{first_card}"
 puts "2nd card: #{second_card}"
 puts "Dealer card: #{dealer_first_card}"
 
-puts ''
+puts '_'*50
 
 puts "User hand type: #{type_of_user_hand}"
 
 ###
 
-puts ''
+puts '_'*50
 
 ###
 
@@ -130,6 +143,8 @@ soft_type_solutions = {
 }
 
 pair_type_solutions = {
+      #2        #3      #4        #5      #6        #7      #8      #9    #10     #11
+      # 0        1      2         3       4         5         6       7     8       9
 4 => ["Split", "Split", "Split", "Split", "Split", "Split", "Hit", "Hit", "Hit", "Hit"],          # 0
 6 => ["Split", "Split", "Split", "Split", "Split", "Split", "Split", "Hit", "Hit", "Hit"],          # 1
 8 => ["Hit", "Hit", "Split", "Split", "Split", "Hit", "Hit", "Hit", "Hit", "Hit"],          # 2
@@ -139,18 +154,32 @@ pair_type_solutions = {
 16 => ["Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split"],         # 6
 18 => ["Split", "Split", "Split", "Split", "Split", "Stand", "Split", "Split", "Stand", "Stand"],         # 7
 20 => ["Stand", "Stand", "Stand", "Stand", "Stand", "Stand", "Stand", "Stand", "Stand", "Stand"],         # 8
-2 => ["Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split"]           # 9
+22 => ["Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split", "Split"]           # 9
 }
 
 ######################
 
 
-chosen_table = choose_table(type_of_user_hand, hard_type_solutions, soft_type_solutions, pair_type_solutions)
+
+chosen_table = choose_tables(type_of_user_hand, hard_type_solutions, soft_type_solutions, pair_type_solutions)
+
+amount = subtract_amount(type_of_user_hand)
 
 keys = chosen_table.keys
-to_find_index = chosen_table[keys[total_card_value-5]]
+
+
+
+
+# I know there is a way to do this in a method but I did this quickly to get the right answer.
+if type_of_user_hand == "Pair"
+  to_find_index = chosen_table[keys[first_card-amount]]
+
+else
+  to_find_index = chosen_table[keys[total_card_value-amount]]
+
+end
 
 
 
 puts "You should: #{to_find_index[dealer_first_card-2]}"
-puts ''
+puts '_'*50
